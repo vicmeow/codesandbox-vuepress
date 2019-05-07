9880b211610a73273df4606bcfaf4e7ce754481e
Nilay Patel <48822794+nilaypatel18@users.noreply.github.com>
2019-05-07 11:26:18 +0530

    blog
    add blog readme filde

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,15 +6,19 @@
 // https://v1.vuepress.vuejs.org/config/
 
 module.exports = {
-  title: 'VuePress + CodeSandbox',
-  description: 'VuePress starter template for CodeSandbox',
-  theme: '@vuepress/vue',
+  title: "VuePress + CodeSandbox = ❤️",
+  description: "VuePress starter template for CodeSandbox",
+  theme: "@vuepress/vue",
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'VuePress', link: 'https://vuepress.vuejs.org/' },
-      { text: 'GitHub', link: 'https://github.com/vicbergquist/codesandbox-vuepress/' },
+      { text: "Home", link: "/" },
+      { text: "Blogs", link: "/blog/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "VuePress", link: "https://vuepress.vuejs.org/" },
+      {
+        text: "GitHub",
+        link: "https://github.com/vicbergquist/codesandbox-vuepress/"
+      }
     ]
   }
-}
+};
